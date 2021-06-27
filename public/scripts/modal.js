8bb8da841d72ad5a811845e1cae021c02ef75dcb
Alexandre Johann <alexandrejohann@gmail.com>
2021-06-27 16:50:03 -0300

    Editado arquivo Modal JS

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,1 +1,21 @@
+export default function Modal(){
 
+    const modalWrapper = document.querySelector('.modal-wrapper');
+    const cancelButton = document.querySelector('.button.cancel');
+
+    cancelButton.addEventListener("click", close);
+
+    function open(){
+        //Funcionalidade para atribuir a classe active no modal
+        modalWrapper.classList.add("active");
+    }
+    function close(){
+        //Funcionalidade para remover a classe active da modal
+        modalWrapper.classList.remove("active");
+    }
+
+    return{
+        open,
+        close
+    }
+}
